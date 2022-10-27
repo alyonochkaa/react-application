ca92fa80dca62dacd641e733db389da20b386176
alyonochkaa <alyonkakhv@gmail.com>
2022-10-27 14:20:04 +0300

    deleted elements

--- src/components/component-1/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import "./style.css"
-
-function Component1 () {
- return (<div className='custom-text'> 
-    <p>Glory to Ukraine! Glory to the Heroes!</p>
- </div>);
-}
-export default Component1; 
