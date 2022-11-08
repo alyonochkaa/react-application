4542fbef613627e0f2ed684276442e33ba6fde6c
alyonochkaa <alyonkakhv@gmail.com>
2022-11-08 14:32:00 +0200

    changes 08/11

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+import React from 'react';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Navigation from './components/navigation';
 import Content from './components/content';
+import About from './components/about';
+import NotFoundPage from './components/404';
 
 function App () {
   return (
     <div className="App">
-      <Navigation />
-      <Content />
+      <BrowserRouter>
+        <Navigation />
+        <Routes>
+          <Route path='/' element={<Content />} />
+          <Route path='/about' element={< About />} />
+          <Route path='*' element={<NotFoundPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
