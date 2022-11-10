02327d3442ac5aa7faaffeef418a6dcba0e34dea
alyonochkaa <alyonkakhv@gmail.com>
2022-11-10 14:24:01 +0200

    deleted image

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useHref } from "react-router-dom";
-import image from './https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.com%2Ffree-photos-vectors%2Flittle-girl-cartoon&psig=AOvVaw05DCobWSta7OWMI4eY79CI&ust=1667998211627000&source=images&cd=vfe&ved=0CA0QjRxqFwoTCNi7oODPnvsCFQAAAAAdAAAAABAJ'
+
 
 function About () {
  return (<div><h1>About us</h1></div>);
